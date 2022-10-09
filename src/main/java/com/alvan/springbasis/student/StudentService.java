1088e413a5f7bd3bcb15e59eccbd7f96add03c78
alvannwanorim <alvannwanorim@gmail.com>
2022-10-09 17:25:14 +0100

    updated ednpoints

--- src/main/java/com/alvan/springbasis/student/StudentService.java
+++ src/main/java/com/alvan/springbasis/student/StudentService.java
@@ -1,6 +1,7 @@
 package com.alvan.springbasis.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,10 @@ public class StudentService {
 	}
 
 	public void createNewStudent(Student student) {
-		System.out.println(student);
+		Optional<Student> studentOptional = studentRepository.findStudentByEmaial(student.getEmail());
+		if(studentOptional.isPresent()){
+			throw new IllegalStateException("Email already exists");
+		}
+		studentRepository.save(student);
 	} 
 }
